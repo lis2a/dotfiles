b41f40dcc5929e027db953d4f312a9b3e57f1bc6
lis2a <me@lilis.dev>
2023-07-03 01:15:18 +0900

    feat: migration homebrew runner

---
+++ homebrew.ts
@@ -0,0 +1,38 @@
+import { defineTask, exec } from "./deps.ts";
+
+const homebrewRunner = defineTask([
+    exec({
+        cmd: 'brew',
+        args: ['--version'],
+    }),
+    // Homebrew Core のアップデート
+    exec({
+        cmd: 'brew',
+        args: ['update'],
+    }),
+    // Brewfile の同期
+    exec({
+        cmd: 'brew',
+        args: ['bundle', '--file', '\'~/Brewfile\''],
+    }),
+    exec({
+        cmd: 'brew',
+        args: ['bundle', 'cleanup', '--force', '--file', '\'~/Brewfile\''],
+    }),
+    // Formula のアップデート
+    exec({
+        cmd: 'brew',
+        args: ['upgrade'],
+    }),
+    // Formula のクリーンアップ
+    exec({
+        cmd: 'brew',
+        args: ['cleanup', '-n'],
+    }),
+    exec({
+        cmd: 'brew',
+        args: ['cleanup'],
+    })
+])
+
+homebrewRunner.run()
